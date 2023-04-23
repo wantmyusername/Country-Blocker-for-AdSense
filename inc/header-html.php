b010f7f39c1c9ca1782029617f227ff763ef6830
Jesús <wantmyusername@users.noreply.github.com>
2023-04-22 21:24:24 -0600

    Update header-html.php

--- inc/header-html.php
+++ inc/header-html.php
@@ -8,19 +8,24 @@ $current_url = parse_url(home_url());
 
 <?php
 
-$access_key = $settings['api_key']; // Reemplaza esto con tu clave de acceso
+$access_key = isset( $settings['api_key'] ) ? wp_kses_post( $settings['api_key'] ) : ''; 
 $ip_address = $_SERVER['REMOTE_ADDR'];
 
-$json = file_get_contents("https://api.ipgeolocation.io/ipgeo?apiKey=$access_key&ip=$ip_address");
-$api = json_decode($json);
+$response = wp_remote_get( "https://api.ipgeolocation.io/ipgeo?apiKey=$access_key&ip=$ip_address" );
 
-$blocked_countries = explode(',', $settings['pais']); // Convertir la cadena de países en un array
-
-
-if (in_array($api->country_code2, $blocked_countries)) { // Verificar si el país está en el array de países bloqueados
-    echo '<!-- Sorry, there are no ads available in your country. Country blocked: ' . $settings['pais'] . ' -->';
+if ( is_wp_error( $response ) ) {
+    echo '<!-- Ups, API call error :( -->';
 } else {
-    echo '<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-' . $settings['id'] . '" crossorigin="anonymous"></script>';
+    $body = wp_remote_retrieve_body( $response );
+    $api = json_decode( $body );
+
+    $blocked_countries = explode(',', isset( $settings['pais'] ) ? wp_kses_post( $settings['pais'] ) : '' ); // Convertir la cadena de países en un array
+
+    if (in_array($api->country_code2, $blocked_countries)) { // Verificar si el país está en el array de países bloqueados
+        echo '<!-- Sorry, there are no ads available in your country. Country blocked: ' . $settings['pais'] . ' -->';
+    } else {
+        echo '<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-' . esc_html( $settings['id'] ) . '" crossorigin="anonymous"></script>';
+    }
 }
 
 ?>
