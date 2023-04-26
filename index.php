0b35381fdb0ad6be17c2c87e30008f46b47f0751
Jesús <wantmyusername@users.noreply.github.com>
2023-04-25 19:50:07 -0600

    Update index.php

--- index.php
+++ index.php
@@ -55,9 +55,9 @@ function CBFA_guardar_cbfa()
 
     $ajax_settings = [];
     
-    $ajax_settings["id"] = intval($_POST["trid"]);
+    $ajax_settings["id"] = sanitize_text_field($_POST["trid"]);
     $ajax_settings["pais"] = sanitize_text_field($_POST["toe"]);
-    $ajax_settings["api_key"] = intval($_POST["apikeyfor"]);
+    $ajax_settings["api_key"] = sanitize_text_field($_POST["apikeyfor"]);
 
     
     $ajax_settings["save"] = time();
